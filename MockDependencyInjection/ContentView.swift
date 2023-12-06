b93bf401153931602980fdd446a1ddeddbd75420
Elaidzha1940 <eli080293@gmail.com>
2023-12-06 09:27:23 +0300

    Add ProductionDataService

--- MockDependencyInjection/ContentView.swift
+++ MockDependencyInjection/ContentView.swift
@@ -8,12 +8,54 @@
 //  */
 
 import SwiftUI
+import Combine
+
+
+// Problems with Singletons:
+//
+// 1. Singleton's are Global.
+// 2. Cant't customize the init.
+// 3. Cant't swap out service.
+
+struct PostModel: Identifiable, Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let bode: String
+}
+
+class ProductionDataService {
+    
+    let url: URL
+    init(url: URL) {
+        self.url = url
+    }
+    
+    func getData() -> AnyPublisher<[PostModel], Error>
+    URLSession.shared.dataTaskPublisher(for: url)
+        .map({ $.0data })
+        .decode(type: [PostModel].self, decoder: JSONDecoder())
+        .receive(on: dispatchQueue.main)
+        .eraseToAnyPublisher()
+}
+
+
+
+struct MockDependencyInjection: View {
+    @StateObject private var vm: MockDependencyInjection
+    
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
 
 struct ContentView: View {
     var body: some View {
         
-        VStack {
-           
+        ScrollView {
+            VStack {
+                
+            }
         }
     }
 }
