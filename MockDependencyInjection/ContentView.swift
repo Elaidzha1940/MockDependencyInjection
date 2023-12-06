826072cc1753e236fb3509902879ad654fa99bc5
Elaidzha1940 <eli080293@gmail.com>
2023-12-06 06:35:43 +0300

    MockDependencyInjection

---
+++ MockDependencyInjection/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: MockDependencyInjection
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 06.12.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+           
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MockDependencyInjection/MockDependencyInjectionApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: MockDependencyInjection
+//  File: MockDependencyInjectionApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 06.12.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct MockDependencyInjectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
